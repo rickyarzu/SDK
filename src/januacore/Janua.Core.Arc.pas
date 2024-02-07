be383495656a01084abeca6de899616f48ca8cdf
Riccardo Arduino <rickyarzu@gmail.com>
2024-02-07 12:15:37 +0100

    Added Compare Ex ARC and ProcQueue Libraries to Framework

---
+++ src/januacore/Janua.Core.Arc.pas
@@ -0,0 +1,125 @@
+/// <summary>
+///   Unit contains the smart pointer implementation which can be used to wrap
+///   the non-interfaced object for automatic memory management (so that they
+///   are released once they leave the scope).
+/// </summary>
+unit Janua.Core.Arc;
+
+interface
+
+(*
+  Once you create the object using the Arc<T> record you do not need to release it manually anymore.
+
+  Example 1:
+  var
+    L: Arc<TStringList>;
+  begin
+    L := TStringList.Create;
+    L.Value.LoadFromFile(...);
+  end;
+
+  Example 2:
+  var
+    L: Arc<TFileStream>;
+  begin
+    L := TFileStream.Create(TFileStream.Create('C:\Path\To\File.ext', fmOpenRead or fmShareDenyNone);
+    L.Value.Read(...);
+  end;
+*)
+
+type
+  /// <summary>
+  ///   Smart record which releases the object once it goes out of scope.
+  /// </summary>
+  Arc<T: class> = record
+  strict private
+    /// <summary>
+    ///   Hold the value for the source object.
+    /// </summary>
+    FValue: T;
+    /// <summary>
+    ///   Holds the reference to interface of the object owner.
+    /// </summary>
+    FObjectOwner: IInterface;
+    /// <summary>
+    ///   Getter for the <c>Value</c> property.
+    /// </summary>
+    function GetThis: T;
+  private type
+    /// <summary>
+    ///   Special object which is releasing the source instance when goes out
+    ///   of scope.
+    /// </summary>
+    TArcHolder = class(TInterfacedObject)
+    private
+      /// <summary>
+      ///   Source object reference.
+      /// </summary>
+      FObject: TObject;
+    public
+      constructor Create(AObject: TObject);
+      destructor Destroy; override;
+    end;
+  public
+    /// <summary>
+    ///   Property to access the source object.
+    /// </summary>
+    property This: T read GetThis;
+
+    /// <summary>
+    ///   Function returns the unmanaged instance and removes the ownership in
+    ///   current record.
+    /// </summary>
+    function Extract: T;
+
+    /// <summary>
+    ///   Implicit conversion from source type.
+    /// </summary>
+    class operator Implicit(AFrom: T): Arc<T>;
+    /// <summary>
+    ///   Implicit conversion from smart record type to source.
+    /// </summary>
+    class operator Implicit(AFrom: Arc<T>): T;
+  end;
+
+implementation
+
+{ TArc<T> }
+
+function Arc<T>.Extract: T;
+begin
+  Result := This;
+  TArcHolder(FObjectOwner).FObject := nil;
+  FObjectOwner := nil;
+end;
+
+function Arc<T>.GetThis: T;
+begin
+  Result := FValue;
+end;
+
+class operator Arc<T>.Implicit(AFrom: T): Arc<T>;
+begin
+  Result.FValue := AFrom;
+  Result.FObjectOwner := TArcHolder.Create(AFrom);
+end;
+
+class operator Arc<T>.Implicit(AFrom: Arc<T>): T;
+begin
+  Result := AFrom.This;
+end;
+
+{ TArc<T>.TFreeTheValue }
+
+constructor Arc<T>.TArcHolder.Create(AObject: TObject);
+begin
+  FObject := AObject;
+end;
+
+destructor Arc<T>.TArcHolder.Destroy;
+begin
+  FObject.Free;
+  inherited;
+end;
+
+end.
---
+++ src/januacore/Janua.Core.ProcQueue.pas
@@ -0,0 +1,219 @@
+unit Janua.Core.ProcQueue;
+
+interface
+
+uses
+  {$IFDEF MSWINDOWS}
+  Vcl.Controls,
+  Winapi.Messages,
+  Winapi.Windows,
+  {$ENDIF}
+  System.Classes,
+  System.SysUtils,
+  System.Generics.Collections;
+
+type
+  /// <summary>
+  ///   Class provides the functionality to queue procedure for later execution in the windows message queue.
+  /// </summary>
+  ProcQueue = class sealed
+  {$IFDEF MSWINDOWS}
+  private type
+    TQueueProcOwner = class;
+
+    /// <summary>
+    ///   Record stores the queue entry.
+    /// </summary>
+    TQueueEntry = record
+      /// <summary>
+      ///   Reference to the procedure.
+      /// </summary>
+      Proc: TProc;
+      /// <summary>
+      ///   Reference
+      /// </summary>
+      Owner: TQueueProcOwner;
+      constructor Create(const AProc: TProc; const AOwner: TComponent);
+    end;
+
+    /// <summary>
+    ///   The component automatically removes the queue entry
+    ///   once the owner gets destroyed.
+    /// </summary>
+    TQueueProcOwner = class(TComponent)
+    private
+      /// <summary>
+      ///   This flag is set when the entry is removed
+      ///   when calling the procedure to avoid redundant
+      ///   check of the list items for removal.
+      /// </summary>
+      FDeletingFromQueue: Boolean;
+    public
+      /// <summary>
+      ///   Releases the object without checking the list.
+      /// </summary>
+      procedure Release;
+      destructor Destroy; override;
+    end;
+  private class threadvar
+    /// <summary>
+    ///   Holds the list of enqueued procedures.
+    /// </summary>
+    FList: TList<TQueueEntry>;
+    /// <summary>
+    ///   The invisible window which processes the messages and
+    ///   calls the queue procedures.
+    /// </summary>
+    FQueueHWND: HWND;
+  {$ENDIF}
+  private
+    {$IFDEF MSWINDOWS}
+    /// <summary>
+    ///   Custom window procedure to process the queue.
+    /// </summary>
+    class procedure WndProc(var AMessage: TMessage);
+    {$ENDIF}
+  public
+    /// <summary>
+    ///   Procedure to enqueue the procedure for later execution in message queue.
+    /// </summary>
+    class procedure Enqueue(const AProc: TProc{$IFDEF MSWINDOWS}; const AOwner: TComponent = nil{$ENDIF});
+    {$IFDEF MSWINDOWS}
+    class destructor Destroy;
+    {$ENDIF}
+  end;
+
+implementation
+
+{$IFNDEF MSWINDOWS}
+uses
+  System.Threading;
+{$ENDIF}
+
+{ ProcQueue }
+
+{$IFDEF MSWINDOWS}
+class destructor ProcQueue.Destroy;
+begin
+  { Release the list and handler }
+  if FQueueHWND <> 0 then
+    DeallocateHWnd(FQueueHWND);
+
+  FList.Free;
+end;
+{$ENDIF}
+
+class procedure ProcQueue.Enqueue(const AProc: TProc{$IFDEF MSWINDOWS}; const AOwner: TComponent{$ENDIF});
+begin
+  {$IFDEF MSWINDOWS}
+  if FList = nil then
+    begin
+      { Lazy create the list ad handler }
+      FList := TList<TQueueEntry>.Create;
+      FQueueHWND := AllocateHWnd(WndProc);
+    end;
+
+  { Add entry to the queue }
+  FList.Add(TQueueEntry.Create(AProc, AOwner));
+
+  { Post the message to the handler }
+  PostMessage(FQueueHWND, WM_USER, 0, 0);
+
+  {$ELSE}
+
+  TTask.Run(
+    procedure
+    begin
+      TThread.Queue(nil,
+        procedure
+        begin
+          AProc;
+        end)
+    end)
+  {$ENDIF}
+end;
+
+{$IFDEF MSWINDOWS}
+destructor ProcQueue.TQueueProcOwner.Destroy;
+var
+  i: Integer;
+begin
+  { If we need to skip the queue check }
+  if FDeletingFromQueue then
+    begin
+      inherited;
+      Exit;
+    end;
+
+  { Remove the corresponding queue entry }
+  i := 0;
+  while i < FList.Count do
+    if FList[i].Owner = Self then
+      begin
+        FList.Delete(i);
+        Break;
+      end
+    else
+      Inc(i);
+
+  inherited;
+end;
+{$ENDIF}
+
+{$IFDEF MSWINDOWS}
+procedure ProcQueue.TQueueProcOwner.Release;
+begin
+  { Check for nil }
+  if Self = nil then
+    Exit;
+
+  { Set the flag to skip check }
+  FDeletingFromQueue := True;
+
+  { Free the instance }
+  Free;
+end;
+{$ENDIF}
+
+{$IFDEF MSWINDOWS}
+constructor ProcQueue.TQueueEntry.Create(const AProc: TProc; const AOwner: TComponent);
+begin
+  Proc := AProc;
+  { Create the handler if owner assigned }
+  if AOwner = nil then
+    Owner := nil
+  else
+    Owner := TQueueProcOwner.Create(AOwner)
+end;
+{$ENDIF}
+
+{$IFDEF MSWINDOWS}
+class procedure ProcQueue.WndProc(var AMessage: TMessage);
+var
+  LEntry: TQueueEntry;
+begin
+  if AMessage.Msg <> WM_USER then
+    begin
+      DefWindowProc(FQueueHWND, AMessage.Msg, AMessage.WParam, AMessage.LParam);
+      Exit;
+    end;
+
+  { If no events queued then exit }
+  if FList.Count = 0 then
+    Exit;
+
+  { Get fist entry }
+  LEntry := FList.First;
+
+  { Delete the entry form the list }
+  FList.Delete(0);
+
+  { Release the handler }
+  LEntry.Owner.Release;
+
+  { Call the enqueued procedure }
+  LEntry.Proc();
+end;
+{$ENDIF}
+
+end.
