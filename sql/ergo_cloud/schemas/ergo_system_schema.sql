3c7b6828863e959d782e2b9b00c6c76448788e2c
Riccardo Arduino <rickyarzu@gmail.com>
2024-03-07 07:14:28 +0100

    Created Cloud Framework and Updated Phoneix App

---
+++ sql/ergo_cloud/schemas/ergo_system_schema.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS ergo_system
+    AUTHORIZATION ergo;
+
+COMMENT ON SCHEMA ergo_system
+    IS 'Thi is the System Schema containing all connection Data and storing passwords and user sessions';
