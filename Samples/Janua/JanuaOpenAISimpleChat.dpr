142b6037847056866d8ac7192c1b4ea75d613dd3
rickyarzu <rickyarzu@gmail.com>
2024-06-16 16:50:41 +0200

    Added OpenAI Support To Developer IDE

---
+++ Samples/Janua/JanuaOpenAISimpleChat.dpr
@@ -0,0 +1,39 @@
+﻿program JanuaOpenAISimpleChat;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  OpenAI.API.Params,
+  OpenAI.API,
+  OpenAI.Audio,
+  OpenAI.Chat.Functions,
+  OpenAI.Chat.Functions.Samples,
+  OpenAI.Chat,
+  OpenAI.Completions,
+  OpenAI.Component.Chat,
+  OpenAI.Edits,
+  OpenAI.Embeddings,
+  OpenAI.Engines,
+  OpenAI.Errors,
+  OpenAI.Files,
+  OpenAI.FineTunes,
+  OpenAI.FineTuning,
+  OpenAI.Images,
+  OpenAI.Models,
+  OpenAI.Moderations,
+  OpenAI,
+  OpenAI.Utils.ChatHistory,
+  OpenAI.Component.Functions,
+  OpenAI.Assistants,
+  OpenAI.Types,
+  OpenAI.Utils.ObjectHolder,
+  OpenAI.Utils.Base64,
+  Janua.FMX.Demos.OpenAI.Chat.Main in 'FMX\Janua.FMX.Demos.OpenAI.Chat.Main.pas' {FormChat};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFormChat, FormChat);
+  Application.Run;
+end.
