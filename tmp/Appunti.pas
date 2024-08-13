472da9d3c6dec88d1a797fbc9024cd8f943d5f8b
Riccardo Arduino <rickyarzu@gmail.com>
2024-08-13 15:35:29 +0200

    Added Planner Images

--- tmp/Appunti.pas
+++ tmp/Appunti.pas
@@ -1,23 +1,12 @@
 unit Janua.Core.WebServer;
 ...
 
-Type
-  TJanuaWebServer = class
-  public
-    constructor Create; overload;
-    /// <summary> Create Class imposta la class Var FPort. Variabile 'unica' in tutta l'applicazione </summary>
-    class Constructor CreateClass;
-  private
-    class var FPort: Integer;
-  private
-    FLogProc: TMessageLogProc;
-  public
-    class function GetPort(aDefault: Integer): Integer; overload;
-    procedure StartServer; overload; virtual; abstract;
-    procedure StopServer; overload; virtual; abstract;
-    procedure WriteStatus; virtual; abstract;
-  public
-    property IsActive: Boolean read GetIsActive write SetIsActive;
-    property LogProc: TMessageLogProc read FLogProc write SetLogProc;
-    class property Port: Integer read GetPort write SetPort;
-  end;
+TJanuaWebBrokerServer = class(TJanuaWebServer)
+private
+  FServer: TIdHTTPWebBrokerBridge;
+...
+end;
+
+TJanuaWebServerClass = class of TJanuaWebServer;
+
+class property WebModuleClass: TComponentClass read FWebModuleClass write FWebModuleClass;
